f87d5a35a0d63de1737ab39af5a85fc20881d8c8
ulrichyv <ulrichyvan40@gmail.com>
2023-10-16 13:37:00 +0100

    vers1

---
+++ addons/om_hospital/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name':'hospital management systeme',
+    'author':'ulrich',
+    'website':'odoo.tech',
+    'sumary':'odoo16 Development',
+    'depends':['mail'],
+    'data':[
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml'
+    ]
+}
